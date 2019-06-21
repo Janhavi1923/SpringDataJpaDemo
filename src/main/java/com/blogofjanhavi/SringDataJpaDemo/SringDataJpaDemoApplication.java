005ac01f550c94cfd833fcb8ba2da7f1edc57556
Janhavi Srivastava <janhavisrivastav230897@gmail.com>
2019-06-21 22:54:28 +0530

    downloaded skeleton from spring intializr

---
+++ src/main/java/com/blogofjanhavi/SringDataJpaDemo/SringDataJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.blogofjanhavi.SringDataJpaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringDataJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringDataJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogofjanhavi/SringDataJpaDemo/SringDataJpaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogofjanhavi.SringDataJpaDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SringDataJpaDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
